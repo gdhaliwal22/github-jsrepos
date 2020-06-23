ce02f788642d0ffc866a50199d6767a21da0bf06
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-06-23 12:15:45 -0700

    typed out pseudocode

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+// as a user, I want to see all javascript repos with over 25k stars, srted by most to least order
+// as a user, I want to see the name, number of starts, and link to the repo
+// as a user I want to know when I have an error
+
+//github api documenation
+
+// https://api.github.com/search/repositories?q=tetris+language:assembly&sort=stars&order=desc
+
+// create a query
+// make an api call on app load
+// parse the data
+// set to state
+// display data in some manner
+// handle loading and error states
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>Hello</div>;
 }
 
 export default App;
