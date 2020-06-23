184f5266f687d8b7ab6dc736ac7010f43d42d75e
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-06-23 12:26:41 -0700

    created fetch api call

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useEffect } from "react";
 import "./App.css";
 
 // as a user, I want to see all javascript repos with over 25k stars, srted by most to least order
@@ -11,13 +10,36 @@ import "./App.css";
 // https://api.github.com/search/repositories?q=tetris+language:assembly&sort=stars&order=desc
 
 // create a query
+
+// https://api.github.com/search/repositories?q=stars:>25000+language:javascript&sort=stars&order=desc
+
+// name, number of stars, link to repo
+
+// name
+// stargazers_url
+// html_url
+// id
+
+function getGithubRepos() {
+  return fetch(
+    "https://api.github.com/search/repositories?q=stars:>25000+language:javascript&sort=stars&order=desc"
+  ).then((data) => data.json());
+}
+
+getGithubRepos();
+
 // make an api call on app load
+//useEffect - make api call
+
 // parse the data
 // set to state
 // display data in some manner
 // handle loading and error states
 
 function App() {
+  useEffect(() => {
+    getGithubRepos().then((res) => console.log(res));
+  }, []);
   return <div>Hello</div>;
 }
 
